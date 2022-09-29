cfeda2354cb319cd33596b002a720431fef13955
Macéo Prévost <maceo.prevost@outlook.fr>
2022-09-29 10:33:41 +0200

    Add Text Mesh Pro

---
+++ Assets/ChoseYourChara.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoseYourChara : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> charas = new List<GameObject>();
+
+
+    
+
+}
