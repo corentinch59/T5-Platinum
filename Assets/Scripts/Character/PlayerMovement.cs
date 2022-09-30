134620af578501c62cbd0283bf8ad5074004a38e
Corentin Chrisment <tintinch59@gmail.com>
2022-09-30 15:14:50 +0200

    added gravity to player

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -9,9 +9,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float playerSpeed;
 
     private CharacterController controller;
-    private Vector2 orientation;
+
+    private const float gravityValue = -9.81f;
 
     private Vector3 moveDir;
+    private Vector3 playerVelocity;
+
+    private Vector2 orientation;
     private Vector2 move;
 
     private void Start()
@@ -22,7 +26,13 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         moveDir = new Vector3(move.x, 0, move.y);
-        controller.Move(moveDir * playerSpeed * Time.deltaTime);
+        controller.Move(moveDir * playerSpeed * Time.fixedDeltaTime);
+
+        if (controller.isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = 0f;
+
+        playerVelocity.y += controller.isGrounded ? 0f : gravityValue * Time.fixedDeltaTime;
+        controller.Move(playerVelocity * Time.fixedDeltaTime);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
