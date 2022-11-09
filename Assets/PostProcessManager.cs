cb42ddd772f6f1bf76e61250345a71008205156a
Firtnn <HectorBrun@outlook.fr>
2022-11-09 11:23:46 +0100

    WIP SunshinePostProcess

--- Assets/PostProcessManager.cs
+++ Assets/PostProcessManager.cs
@@ -11,6 +11,8 @@ public class PostProcessManager : MonoBehaviour
     public Volume _volume;
     private Vignette _vignette;
     private FilmGrain _filmGrain;
+    public float vignetteEndIntensity;
+    public float filmGrainEndIntensity;
 
     private void Start()
     {
@@ -19,7 +21,6 @@ public class PostProcessManager : MonoBehaviour
         _volume.profile.TryGet(out _filmGrain);
         _vignette.intensity.value = 0;
         _filmGrain.intensity.value = 0;
-        
     }
 
     private void Update()
@@ -30,6 +31,19 @@ public class PostProcessManager : MonoBehaviour
             _filmGrain.intensity.value = 0.8f;
         }
     }
-    
+
+    public IEnumerator SunshinePostProcess(float lerpDuration)
+    {
+        float timeElapsed = 0;
+        while (timeElapsed < lerpDuration)
+        {
+            _vignette.intensity.value = Mathf.Lerp(0, vignetteEndIntensity, timeElapsed / lerpDuration);
+            _filmGrain.intensity.value = Mathf.Lerp(0, filmGrainEndIntensity, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        _vignette.intensity.value = vignetteEndIntensity;
+        _filmGrain.intensity.value = filmGrainEndIntensity;
+    }
     
 }
