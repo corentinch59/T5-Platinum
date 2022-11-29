203d5497de2dee88d97c81d2c1b16814a488bf3c
Freddy <freddy_maupou@orange.fr>
2022-11-29 15:27:14 +0100

    [FIX] Player can only interact with hole while he's carrying a body

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -57,7 +57,7 @@ public class SoundManager : MonoBehaviour
 		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
 
 		s.source.Play();
-		Debug.Log("Played :" + " " + s.name);
+		//Debug.Log("Played :" + " " + s.name);
 		return s;
 	}
 
