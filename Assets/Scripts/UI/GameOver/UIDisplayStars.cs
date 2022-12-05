16f3e3e89ba4518b120ba53b82493d6655ea462e
Macéo Prévost <maceo.prevost@outlook.fr>
2022-12-05 14:41:49 +0100

    [Add] GameOver Panel

--- Assets/Scripts/UI/GameOver/UIDisplayStars.cs
+++ Assets/Scripts/UI/GameOver/UIDisplayStars.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIDisplayStars : MonoBehaviour
 {
-
+    [SerializeField] private Slider satisfactionSlider;
     private List<GameObject> stars = new List<GameObject>();
-
 
     private void Start()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
             stars.Add(transform.GetChild(i).gameObject);
+
+            transform.GetChild(i).gameObject.SetActive(false);
+            Debug.Log("Stars");
         }
 
-        
+        DisplayStars();
+    }
+
+    private void DisplayStars()
+    {
+        Debug.Log("Voir avec les GD pour comment on affiche le score");
     }
 }
