42bc79d0be3dacb580166edce090cd49427f2855
Macéo Prévost <maceo.prevost@outlook.fr>
2022-12-09 10:53:09 +0100

    [Tweeks]

--- Assets/Scripts/PNJ/QuestManager.cs
+++ Assets/Scripts/PNJ/QuestManager.cs
@@ -22,7 +22,7 @@ public class QuestManager : MonoBehaviour
     [Header("Score")]
     public float score;
     public const float failComboToAdd = 2.5f;
-    public const float scoreToAdd = 1.5f;
+    public const float scoreToAdd = 3f;
     public const float scoreToRemove = 1.5f;
     private float failCombo = 0f;
 
